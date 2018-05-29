c926fd6d10b9fb88047bafc87879eb8e23d6d958
zack <wb-zhangchuang@zhongan.io>
2018-05-29 18:42:16 +0800

    Site updated: 2018-05-29 18:42:15

---
+++ dist/music.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "和煦的糖果风",
+        artist: 'Candy_Wind',
+        url: 'http://music.163.com/song/media/outer/url?id=439142564',
+        cover: 'http://p1.music.126.net/W17K-JoGcG5ldwHK-wttVQ==/109951162805729020.jpg',
+      },
+      {
+        name: 'Bloom of Youth',
+        artist: '清水準一',
+        url: 'http://music.163.com/song/media/outer/url?id=760058',
+        cover: 'http://p1.music.126.net/BxiuK1_8_RXArdjxOqqUeg==/3382097775018074.jpg',
+      }
+    ]
+});
